119bd1e0bbb8f83bc7ed45645bfc6b8bc734d06e
Argenis Chirinos <Argenis_Chirinos@gap.com>
2019-04-22 01:40:48 -0400

    Add Multi-Module project configuration. Closes #1

---
+++ petclinic-web/src/test/java/guru/springframework/petclinic/controllers/DummyController.java
@@ -0,0 +1,10 @@
+package guru.springframework.petclinic.controllers;
+
+import guru.springframework.petclinic.models.Vet;
+
+public class DummyController {
+
+    public void dummy() {
+        Vet vet;
+    }
+}
