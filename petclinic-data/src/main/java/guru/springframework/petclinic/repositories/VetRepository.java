87dee9e76f85b4ffc437f85bcbcc089ad2e9ef44
Argenis Chirinos <Argenis_Chirinos@gap.com>
2019-09-04 20:57:04 -0400

    Add spring jpa vet service. Closes #42

---
+++ petclinic-data/src/main/java/guru/springframework/petclinic/repositories/VetRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.petclinic.repositories;
+
+import guru.springframework.petclinic.models.Vet;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VetRepository extends CrudRepository<Vet, Long> {
+}
